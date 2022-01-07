50d77ae202454e6f90e582cd9ac3d8625d12b0f0
hongling <duck83521@gmail.com>
2022-01-07 22:54:03 +0800

    make it better

--- app.js
+++ app.js
@@ -5,21 +5,40 @@ const http = require("http");
 //const https = require("https")
 const app = express();
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 //const request = require("request");
 
 const list = [{ id: 1, name: "小明" }];
 // 設置請求路徑為 /products 請求方法為 get
+//app.get("/", function (req, res, next) {
+//  var obj = getPrizeFile();
+//  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1')
+//  res.send({ success: true, obj }).end();
+//});
+//首頁
 app.get("/", function (req, res, next) {
-  var obj = getPrizeFile();
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1')
-  res.send({ success: true, obj }).end();
+  return promise
+    .then((obj) => {
+      res.redirect("/index");
+    })
+    .catch((err) => {
+      res.redirect("/auth");
+    })
 });
-
-var server = app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+//首頁
+app.get('/index', function (req, res, next) {
+	
+	var obj = getPrizeFile();
+	res.json({'arr':obj});
+  //res.render('index');
 });
-
+/**
+ * create server
+ */
+server = http.createServer(app)
+server.listen(port, () => {
+  console.log(`server listen to http://localhost:${port}`)
+})
 
 function getPrizeFile() {
   var list = fs.readFileSync('./prizeList.txt', 'utf8');
