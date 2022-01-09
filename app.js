2f060c69959a4102c8c8eb58b3831accebbd0199
hongling <duck83521@gmail.com>
2022-01-09 09:13:41 +0800

    make it better

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 'use strict';
-
 const express = require('express');
-const http = require("http");
+const exphbs  = require('express-handlebars')
+const http    = require("http");
 //const https = require("https")
-const app = express();
-const fs = require('fs');
-const port = process.env.PORT || 3000;
+const app     = express();
+const fs      = require('fs');
+const port    = process.env.PORT || 3000;
 //const request = require("request");
 
 const list = [{ id: 1, name: "小明" }];
 
-//首頁
+/**
+ * 設定 template engine
+ */
+//app.engine('handlebars', exphbs('defaultLayout: main'))
+app.engine('handlebars', require('exphbs'));
+app.set('view engine', 'handlebars')
+
+
+/**
+ * 首頁
+ */
 app.get("/", function (req, res, next) {
 	var obj = getPrizeFile();
 	res.setHeader("Access-Control-Allow-Origin","*");
 	res.json({'arr':obj});
 });
-//首頁
-app.get('/index', function (req, res, next) {
-	
 
+/**
+ * 抽獎頁面
+ */
+app.get('/index', function (req, res, next) {
   res.render('index');
+  
 });
 /**
  * create server
