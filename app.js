de890e491701d340523a2d1dbdd18ae54bb56190
yee <qsw7755896@gmail.com>
2022-01-10 19:54:48 +0800

    make it better

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post("/append", function (req, res, next) {
   var data = "";
 
  
-  console.log(req.body);
+  console.log(req.body.insert);
   // fs.writeFile('./prizeList.txt', data, function (err) {
   //   if (err)
   //     console.log(err);
